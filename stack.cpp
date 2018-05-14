2916bdb0335fd24c360129884c005e5936585c42
rekaamelia014 <38060232+rekaamelia014@users.noreply.github.com>
2018-05-15 05:19:21 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -17,3 +17,20 @@ struct Stack* createStack( unsigned capacity ){
     if (!stack->array) return NULL;
     return stack;
 }
+ 
+int isEmpty(struct Stack* stack){
+    return stack->top == -1 ;
+}
+ char peek(struct Stack* stack)
+{
+    return stack->array[stack->top];
+}
+ char pop(struct Stack* stack){
+    if (!isEmpty(stack))
+    return stack->array[stack->top--] ;
+    return '$';
+}
+ void push(struct Stack* stack, char op){
+    stack->array[++stack->top] = op;
+}
+ 
