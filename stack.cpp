c6b808ef0db44751a5fa12d79b539f8ede493483
naurahnazhifah <38009377+naurahnazhifah@users.noreply.github.com>
2018-05-15 05:18:09 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,8 +1,19 @@
-if (isdigit(exp[i]))
-            push(stack, exp[i] - '0');
-        else{
-            int val1 = pop(stack);
-            int val2 = pop(stack);
-            switch (exp[i])  {
-             case '+': push(stack, val2 + val1); break;
-             case '-': p
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+ 
+struct Stack{
+    int top;
+    unsigned capacity;
+    int* array;
+};
+struct Stack* createStack( unsigned capacity ){
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
+    if (!stack) return NULL;
+    stack->top = -1;
+    stack->capacity = capacity;
+    stack->array = (int*) malloc(stack->capacity * sizeof(int));
+    if (!stack->array) return NULL;
+    return stack;
+}
